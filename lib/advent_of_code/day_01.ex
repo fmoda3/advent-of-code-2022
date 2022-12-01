63234a716ef571df858c3711ff0db128239b896a
Frank Moda <fmoda3@mac.com>
2022-12-01 10:12:27 -0500

    Day 1 Solution

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    args
+    |> parse_input()
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.max()
   end
 
   def part2(args) do
+    args
+    |> parse_input()
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  def parse_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n\n")
+    |> Enum.map(fn x -> String.split(x, "\n") end)
+    |> Enum.map(fn x -> Enum.map(x, &String.to_integer/1) end)
   end
 end
